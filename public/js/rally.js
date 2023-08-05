6485cb73b959bd05d814fb1f8b1101718d2fe47a
Fransiscus Gultom <fransiscus.gultom@praisindo.co.id>
2023-08-05 08:01:19 +0700

    alert

--- public/js/rally.js
+++ public/js/rally.js
@@ -12,6 +12,8 @@ var RallyScript = function() {
     // Setup module components
     var _componentJs = function(){
         $(".btn-cancel").click(function(){
+            alert(main_url)
+            alert($(this).data("method"))
             var meth = $(this).data("method");
             swalInit.fire({
                 title: "Are you sure?",
