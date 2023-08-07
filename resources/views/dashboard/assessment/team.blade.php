afd162a9286261b1149406769688ebb2a473421a
Fransiscus Gultom <fransiscus.gultom@praisindo.co.id>
2023-08-07 21:28:46 +0700

    t

---
+++ resources/views/dashboard/assessment/team.blade.php
@@ -0,0 +1,21 @@
+<div class="card table-responsive">
+    <table id="assessment{{ $classCode }}Table" class="table table-striped">
+        <thead>
+            <tr>
+                <th colspan="3">Team</th>
+            </tr>
+            <tr>
+                <th colspan="3">Team</th>
+                <th colspan="3">Individu</th>
+            </tr>
+            <tr>
+                <th>Rank</th>
+                <th>Team</th>
+                <th>Score</th>
+                <th>Rank</th>
+                <th>Participant</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+    </table>
+</div>
--- resources/views/dashboard/content.blade.php
+++ resources/views/dashboard/content.blade.php
@@ -1,4 +1,18 @@
 @extends('layout.public')
 @section('content')
-    @include('dashboard.assessment_team')
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card table-responsive">
+                @php $classCode = 'CCY' @endphp
+                @include('dashboard.assessment')
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card table-responsive">                
+                @php $classCode = 'DLP' @endphp
+                @include('dashboard.assessment')
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
