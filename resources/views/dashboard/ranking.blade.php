2c412fad91c727a11f5dd3a33f85e86e93c0d649
Fransiscus Gultom <fransiscus.gultom@praisindo.co.id>
2023-08-09 22:03:57 +0700

    t

--- resources/views/dashboard/ranking.blade.php
+++ resources/views/dashboard/ranking.blade.php
@@ -1,58 +1,59 @@
 @extends('layout.public')
 @section('content')
 <div class="card table-responsive">
-    <table class="table table-striped">
+    <table class="table table-striped table-bordered">
         <thead class="text-center">
             <tr>
                 <th rowspan="4" colspan="3">OFFICIAL RESULT</th>
-                <th>TOTAL</th>
-                <th>START</th>
-                <th colspan="3">POS 1</th>
-                <th colspan="3">POS 2</th>
-                <th colspan="3">POS 3</th>
-                <th colspan="3">FINISH</th>
-                <th>TOTAL</th>
-                <th>SCORE</th>
+                <th class="py-1">TOTAL</th>
+                <th class="py-1">START</th>
+                <th class="py-1" colspan="3">POS 1</th>
+                <th class="py-1"colspan="3">POS 2</th>
+                <th class="py-1" colspan="3">POS 3</th>
+                <th class="py-1" colspan="3">FINISH</th>
+                <th class="py-1">TOTAL</th>
+                <th class="py-1">SCORE</th>
             </tr>
             <tr>
-                <th>SCORE</th>
-                <th>TIME</th>
-                <th>TIME</th>
-                <th>TEMPUH</th>
-                <th>SCORE</th>
-                <th>TIME</th>
-                <th>TEMPUH</th>
-                <th>SCORE</th>
-                <th>TIME</th>
-                <th>TEMPUH</th>
-                <th>SCORE</th>
-                <th>TIME</th>
-                <th>TEMPUH</th>
-                <th>SCORE</th>
-                <th class="text-nowrap">WAKTU TEMPUH</th>
-                <th class="text-nowrap">WAKTU TOTAL</th>
+                <th class="py-1">SCORE</th>
+                <th class="py-1">TIME</th>
+                <th class="py-1">TIME</th>
+                <th class="py-1">TEMPUH</th>
+                <th class="py-1">SCORE</th>
+                <th class="py-1">TIME</th>
+                <th class="py-1">TEMPUH</th>
+                <th class="py-1">SCORE</th>
+                <th class="py-1">TIME</th>
+                <th class="py-1">TEMPUH</th>
+                <th class="py-1">SCORE</th>
+                <th class="py-1">TIME</th>
+                <th class="py-1">TEMPUH</th>
+                <th class="py-1">SCORE</th>
+                <th class="text-nowrap py-1">WAKTU TEMPUH</th>
+                <th class="text-nowrap py-1">WAKTU TOTAL</th>
             </tr>
             <tr>
                 @foreach (range('A','P') as $v)
-                <th>{{ $v }}</th>
+                <th class="py-1">{{ $v }}</th>
                 @endforeach
             </tr>
             <tr>
-                <th>E+H+K+N+P</th>
-                <th></th>
-                <th></th>
-                <th>C-B</th>
-                <th></th>
-                <th></th>
-                <th>F-C</th>
-                <th></th>
-                <th>I-F</th>
-                <th></th>
-                <th></th>
-                <th>P-M</th>
-                <th></th>
-                <th>D+G+J+M</th>
-                <th></th>
+                <th class="py-1">E+H+K+N+P</th>
+                <th class="py-1"></th>
+                <th class="py-1"></th>
+                <th class="py-1">C-B</th>
+                <th class="py-1"></th>
+                <th class="py-1"></th>
+                <th class="py-1">F-C</th>
+                <th class="py-1"></th>
+                <th class="py-1"></th>
+                <th class="py-1">I-F</th>
+                <th class="py-1"></th>
+                <th class="py-1"></th>
+                <th class="py-1">P-M</th>
+                <th class="py-1"></th>
+                <th class="py-1">D+G+J+M</th>
+                <th class="py-1"></th>
             </tr>
         </thead>
         <tbody>
